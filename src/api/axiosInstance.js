eab419a881485ce1b66d305724a5ce8f741e3366
nftpro1212 <fozilkhalimov95@gmail.com>
2025-10-09 08:17:44 +0500

    Update axiosInstance.js

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
+// 🔹 Backend manziling
 const API = axios.create({
   baseURL: "https://backend-m6u1.onrender.com",
 });
 
-// 🔹 Telegram login
-export const telegramLogin = (userData) => API.post("/auth/login", userData);
+// 🔹 Telegram login funksiyasi
+export const telegramLogin = (userData) => API.post("/api/auth/login", userData);
 
 export default API;
