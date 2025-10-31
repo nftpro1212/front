f5331446beb49b9f4f45549e077602c2bd31a5ff
nftpro1212 <fozilkhalimov95@gmail.com>
2025-10-31 19:09:58 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -131,7 +131,7 @@ export default function Home() {
               }`}
             >
               {isPremium
-                ? "Premium foydalanuvchilar uchun o‘yin"
+                ? ""
                 :"" }
             </div>
           </div>
