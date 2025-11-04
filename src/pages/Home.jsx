03e9534344658fb69955b13380b5afaea9a5848b
nftpro1212 <fozilkhalimov95@gmail.com>
2025-11-04 06:25:03 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,7 @@ export default function Home() {
 
     const loginUser = async () => {
       try {
-        const res = await API.post("/login", {
+        const res = await API.post("/auth/login", {
           telegramId: telegramUser.id,
           username: telegramUser.username,
           first_name: telegramUser.first_name,
